c000f5e7bb4e5daa5dfec760b0a7f5a138549a99
sebastianpineda <sebastianpineda95@gmail.com>
2024-08-15 16:19:20 -0400

    Update test scripts

--- test/test_build_unix.sh
+++ test/test_build_unix.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd ${SCRIPT_DIR}/..
+
 cd build_test
 
 cmake --build . --parallel 6
--- test/test_config_mac.sh
+++ test/test_config_mac.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd ${SCRIPT_DIR}/..
+
 mkdir -p build_test
 cd build_test
 
