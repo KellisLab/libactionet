ca71d4ab07f706002f62fa1d8b951a1bba59b5d1
sebastianpineda <sebastianpineda95@gmail.com>
2024-08-15 22:03:39 +0000

    Test R build

--- test/test_build_unix.sh
+++ test/test_build_unix.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd ${SCRIPT_DIR}/..
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+cd "${SCRIPT_DIR}"/.. || exit
 
-cd build_test
+cd build_test || exit
 
 cmake --build . --parallel 6
