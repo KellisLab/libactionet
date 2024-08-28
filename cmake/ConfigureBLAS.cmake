84dc85a736d5dd1555ee93273167d86ad0c757bf
sebastianpineda <sebastianpineda95@gmail.com>
2024-08-28 16:27:20 -0700

    Improved BLAS linking. New cmake modules.

--- cmake/ConfigureBLAS.cmake
+++ cmake/ConfigureBLAS.cmake
@@ -27,17 +27,16 @@ The following variables control the behaviour of this module:
 
 macro(CONFIGURE_BLAS_MKL)
     message(STATUS "Using Intel MKL for BLAS and LAPACK")
+
+    ## Check if MKL is active
     if (NOT DEFINED ENV{MKLROOT})
         message(FATAL_ERROR "Using Intel MKL but `MKLROOT` not defined")
     endif ()
 
+    ## Find optional MKL headers
     set(BLAS_HEADERS_USE "$ENV{MKLROOT}/include")
     if (EXISTS "${BLAS_HEADERS_USE}")
         message(STATUS "MKL headers: ${BLAS_HEADERS_USE}")
-        #        target_include_directories(
-        #                actionet
-        #                PRIVATE "${MKL_HEADERS}"
-        #        )
         add_compile_definitions(LIBACTIONET_BLAS_MKL)
     else ()
         message("Not using MKL headers")
@@ -48,33 +47,25 @@ macro(CONFIGURE_BLAS_ACCELERATE)
     if (NOT APPLE)
         message(FATAL_ERROR "Accelerate can only be used on macOS")
     endif ()
+
     ## Find and link Apple BLAS
     message(STATUS "Using Apple Accelerate for BLAS and LAPACK")
     add_compile_definitions(LIBACTIONET_BLAS_ACCELERATE)
 
+    ## Find required Accelerate headers
     set(BLAS_HEADERS_USE "${BLAS_LIBRARIES}/Frameworks/vecLib.framework/Headers")
     if (NOT EXISTS "${BLAS_HEADERS_USE}")
         message(FATAL_ERROR "Cannot locate Apple Accelerate headers")
     endif ()
-
     message(STATUS "Accelerate headers: ${BLAS_HEADERS_USE}")
-    #    target_include_directories(
-    #            actionet
-    #            PRIVATE "${BLAS_HEADERS_USE}"
-    #    )
-    # TODO: Detect Apple architecture. Currently assumes and requires arm64.
-    #    add_compile_options(-flax-vector-conversions -framework Accelerate)
+
+    ## Set required compiler options
     add_compile_options(-framework Accelerate)
     #    TODO: Probably unneeded. Suppress deprecation warnings.
     add_compile_definitions(ACCELERATE_NEW_LAPACK ACCELERATE_LAPACK_ILP64)
 endmacro()
 
-## Configure BLAS/LAPACK
-macro(CONFIGURE_BLAS)
-    message(NOTICE "Configuring BLAS/LAPACK")
-    find_package(BLAS REQUIRED) ## Find BLAS
-    find_package(LAPACK REQUIRED) ## Find LAPACK
-
+macro(CONFIGURE_BLAS_DEPENDS)
     if ((DEFINED BLA_VENDOR) AND (NOT ${BLA_VENDOR} STREQUAL "All"))
         message(STATUS "Using provided BLA_VENDOR")
         if ("${BLA_VENDOR}" MATCHES "Intel")
@@ -95,9 +86,20 @@ macro(CONFIGURE_BLAS)
         endforeach ()
     endif ()
 
-    target_include_directories(
-            actionet
-            PRIVATE "${BLAS_HEADERS_USE}"
-    )
+    if (DEFINED BLAS_HEADERS_USE)
+        target_include_directories(
+                actionet
+                PRIVATE "${BLAS_HEADERS_USE}"
+        )
+    endif ()
 
+endmacro()
+
+## Configure BLAS/LAPACK
+macro(CONFIGURE_BLAS)
+    message(NOTICE "Configuring BLAS/LAPACK")
+    find_package(BLAS REQUIRED) ## Find BLAS
+    find_package(LAPACK REQUIRED) ## Find LAPACK
+
+    CONFIGURE_BLAS_DEPENDS()
 endmacro()
