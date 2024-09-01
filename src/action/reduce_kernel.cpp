2c31a201346979d794122c01ade610f2286cb704
sebastianpineda <sebastianpineda95@gmail.com>
2024-08-31 21:01:52 -0700

    Remove old code

--- src/action/reduce_kernel.cpp
+++ src/action/reduce_kernel.cpp
@@ -59,36 +59,3 @@ namespace actionet {
     template arma::field<arma::mat> reduce_kernel<arma::sp_mat>(arma::sp_mat& S, int dim, int svd_alg,
                                                                 int iter, int seed, int verbose);
 } // namespace actionet
-
-
-// template <typename T>
-// arma::field<arma::mat> reduce_kernel_old(T& S, int dim, int svd_alg, int max_it, int seed,
-//                                          int verbose) {
-//     if (verbose) {
-//         stdout_printf("Computing reduced ACTION kernel:\n");
-//         FLUSH;
-//     }
-//
-//     arma::field<arma::mat> SVD_results(3);
-//
-//     SVD_results = runSVD(S, dim, max_it, seed, svd_alg, verbose);
-//
-//     // Update 1: Orthogonalize columns w.r.t. background (mean)
-//     arma::vec mu = arma::vec(arma::mean(S, 1));
-//     arma::vec a1 = mu / arma::norm(mu, 2);
-//     // arma::vec a1 = v;
-//     arma::vec b1 = -arma::trans(S) * a1;
-//
-//     // Update 2: Center columns of orthogonalized matrix before performing SVD
-//     arma::vec c = arma::vec(arma::trans(arma::mean(S, 0)));
-//     double a1_mean = arma::mean(a1);
-//     arma::vec a2 = arma::ones(S.n_rows);
-//     arma::vec b2 = -(a1_mean * b1 + c);
-//
-//     arma::mat A = arma::join_rows(a1, a2);
-//     arma::mat B = arma::join_rows(b1, b2);
-//
-//     arma::field<arma::mat> reduction = actionet::perturbedSVD(SVD_results, A, B);
-//
-//     return reduction;
-// }
