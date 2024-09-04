cc181e64cd75158e8b8e196f606b9d600df732b3
sebastianpineda <sebastianpineda95@gmail.com>
2024-09-04 16:05:27 -0400

    openmp test

--- src/action/action_decomp.cpp
+++ src/action/action_decomp.cpp
@@ -1,6 +1,7 @@
 #include "action/action_decomp.hpp"
 #include "action/spa.hpp"
 #include "action/aa.hpp"
+#include <omp.h>
 #include "tools/normalization.hpp"
 #include "utils_internal/utils_parallel.hpp"
 
@@ -11,9 +12,6 @@ namespace actionet {
         if (thread_no <= 0) {
             thread_no = SYS_THREADS_DEF;
         }
-
-        stdout_printf("Running ACTION (%d threads):", thread_no);
-        FLUSH;
 
         if (k_max == -1)
             k_max = (int) S_r.n_cols;
@@ -27,8 +25,12 @@ namespace actionet {
         trace.C = arma::field<arma::mat>(k_max + 1);
         trace.selected_cols = arma::field<arma::uvec>(k_max + 1);
 
-        // ATTENTION!
+        // TODO: ???
         arma::mat X_r = normalize_matrix(S_r, normalization, 0);
+
+        thread_no = (thread_no == 0) ? std::min(SYS_THREADS_DEF, k_max-k_min+1) : std::min(thread_no, SYS_THREADS_DEF + 2);
+        stdout_printf("Running ACTION (%d threads):", thread_no);
+        FLUSH;
 
         int current_k = 0;
         char status_msg[50];
@@ -37,23 +39,39 @@ namespace actionet {
         stderr_printf("\n\t%s %d/%d finished", status_msg, current_k, (k_max - k_min + 1));
         FLUSH;
 
-        mini_thread::parallelFor(
-                k_min, k_max + 1,
-                [&](size_t kk) {
-                    ResSPA SPA_res = run_SPA(X_r, kk);
-                    trace.selected_cols[kk] = SPA_res.selected_cols;
+        #pragma omp parallel for num_threads(thread_no)
+        for (int k = k_min; k <= k_max; ++k) {
+            ResSPA SPA_res = run_SPA(X_r, k);
+            trace.selected_cols[k] = SPA_res.selected_cols;
 
-                    arma::mat W = X_r.cols(trace.selected_cols[kk]);
+            arma::mat W = X_r.cols(trace.selected_cols[k]);
 
-                    arma::field<arma::mat> AA_res = run_AA(X_r, W, max_it, tol);
-                    trace.C[kk] = AA_res(0);
-                    trace.H[kk] = AA_res(1);
-                    current_k++;
+            arma::field<arma::mat> AA_res = run_AA(X_r, W, max_it, tol);
+            trace.C[k] = AA_res(0);
+            trace.H[k] = AA_res(1);
+            current_k++;
 
-                    stderr_printf("\r\t%s %d/%d finished", status_msg, current_k, (k_max - k_min + 1));
-                    FLUSH;
-                },
-                thread_no);
+            stderr_printf("\r\t%s %d/%d finished", status_msg, current_k, (k_max - k_min + 1));
+            FLUSH;
+        }
+
+        // mini_thread::parallelFor(
+        //         k_min, k_max + 1,
+        //         [&](size_t kk) {
+        //             ResSPA SPA_res = run_SPA(X_r, kk);
+        //             trace.selected_cols[kk] = SPA_res.selected_cols;
+        //
+        //             arma::mat W = X_r.cols(trace.selected_cols[kk]);
+        //
+        //             arma::field<arma::mat> AA_res = run_AA(X_r, W, max_it, tol);
+        //             trace.C[kk] = AA_res(0);
+        //             trace.H[kk] = AA_res(1);
+        //             current_k++;
+        //
+        //             stderr_printf("\r\t%s %d/%d finished", status_msg, current_k, (k_max - k_min + 1));
+        //             FLUSH;
+        //         },
+        //         thread_no);
 
         stdout_printf("\r\t%s %d/%d finished\n", status_msg, current_k, (k_max - k_min + 1));
         FLUSH;
