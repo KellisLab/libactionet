f70697e897ed8c11fd5ec2e65dd294f3fb93e815
sebastianpineda <sebastianpineda95@gmail.com>
2024-09-05 00:46:59 -0400

    OpenMP test

--- include/utils_internal/utils_parallel.hpp
+++ include/utils_internal/utils_parallel.hpp
@@ -2,21 +2,32 @@
 #define ACTIONET_UTILS_PARALLEL_HPP
 
 #include "mini_thread/mini_thread.h"
+#include <omp.h>
 #include <thread>
 // #include <cstddef>
 
 // #define SYS_THREADS_DEF (std::thread::hardware_concurrency() - 2)
 
-inline int get_max_threads()
-{
+inline int get_max_threads() {
     int coresDet = std::thread::hardware_concurrency();
-    if (coresDet >= 6)
-    {
+    if (coresDet >= 6) {
         coresDet -= 2;
     }
     return (coresDet);
 }
 
 const int SYS_THREADS_DEF = get_max_threads();
+
+inline int get_num_threads(int max_threads, int thread_no = 0) {
+    int threads_use;
+    if (thread_no == 0) {
+        threads_use = std::min(SYS_THREADS_DEF, max_threads);
+    }
+    else {
+        threads_use = std::min(thread_no, SYS_THREADS_DEF + 2);
+    }
+
+    return (threads_use);
+}
 
 #endif //ACTIONET_UTILS_PARALLEL_HPP
--- src/action/action_decomp.cpp
+++ src/action/action_decomp.cpp
@@ -5,18 +5,13 @@
 #include "utils_internal/utils_parallel.hpp"
 
 namespace actionet {
-
     ResACTION
-    run_ACTION(arma::mat &S_r, int k_min, int k_max, int normalization, int max_it, double tol, int thread_no) {
-        // if (thread_no <= 0) {
-        //     thread_no = SYS_THREADS_DEF;
-        // }
-
+        run_ACTION(arma::mat& S_r, int k_min, int k_max, int normalization, int max_it, double tol, int thread_no) {
         if (k_max == -1)
-            k_max = (int) S_r.n_cols;
+            k_max = (int)S_r.n_cols;
 
         k_min = std::max(k_min, 2);
-        k_max = std::min(k_max, (int) S_r.n_cols);
+        k_max = std::min(k_max, (int)S_r.n_cols);
 
         ResACTION trace;
 
@@ -27,18 +22,20 @@ namespace actionet {
         // TODO: ???
         arma::mat X_r = normalize_matrix(S_r, normalization, 0);
 
-        thread_no = (thread_no == 0) ? std::min(SYS_THREADS_DEF, k_max-k_min+1) : std::min(thread_no, SYS_THREADS_DEF + 2);
-        stdout_printf("Running ACTION (%d threads):", thread_no);
+        int k_tot = k_max - k_min + 1;
+        int threads_use = get_num_threads(k_tot, thread_no);
+
+        stdout_printf("Running ACTION (%d threads):\n", threads_use);
         FLUSH;
 
         int current_k = 0;
         char status_msg[50];
 
         snprintf(status_msg, 50, "Iterating from k = %d ... %d:", k_min, k_max);
-        stderr_printf("\n\t%s %d/%d finished", status_msg, current_k, (k_max - k_min + 1));
-        FLUSH;
+        // stderr_printf("\n\t%s %d/%d finished", status_msg, current_k, k_tot);
+        // FLUSH;
 
-        #pragma omp parallel for num_threads(thread_no)
+        #pragma omp parallel for num_threads(threads_use)
         for (int k = k_min; k <= k_max; k++) {
             ResSPA SPA_res = run_SPA(X_r, k);
             trace.selected_cols[k] = SPA_res.selected_cols;
@@ -50,32 +47,13 @@ namespace actionet {
             trace.H[k] = AA_res(1);
             current_k++;
 
-            stderr_printf("\r\t%s %d/%d finished", status_msg, current_k, (k_max - k_min + 1));
+            stderr_printf("\r\t%s %d/%d finished", status_msg, current_k, k_tot);
             FLUSH;
         }
 
-        // mini_thread::parallelFor(
-        //         k_min, k_max + 1,
-        //         [&](size_t kk) {
-        //             ResSPA SPA_res = run_SPA(X_r, kk);
-        //             trace.selected_cols[kk] = SPA_res.selected_cols;
-        //
-        //             arma::mat W = X_r.cols(trace.selected_cols[kk]);
-        //
-        //             arma::field<arma::mat> AA_res = run_AA(X_r, W, max_it, tol);
-        //             trace.C[kk] = AA_res(0);
-        //             trace.H[kk] = AA_res(1);
-        //             current_k++;
-        //
-        //             stderr_printf("\r\t%s %d/%d finished", status_msg, current_k, (k_max - k_min + 1));
-        //             FLUSH;
-        //         },
-        //         thread_no);
-
-        stdout_printf("\r\t%s %d/%d finished\n", status_msg, current_k, (k_max - k_min + 1));
+        stdout_printf("\r\t%s %d/%d finished\n", status_msg, current_k, k_tot);
         FLUSH;
 
         return trace;
     }
-
 } // namespace actionet
