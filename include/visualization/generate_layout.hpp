1956befff68353526a15521d2c35f3a63eb6908f
sebastianpineda <sebastianpineda95@gmail.com>
2024-09-06 00:27:30 -0400

    Remove unused definitions

--- include/visualization/generate_layout.hpp
+++ include/visualization/generate_layout.hpp
@@ -5,8 +5,6 @@
 
 // Constants
 #define NEGATIVE_SAMPLE_RATE 3.0
-#define UMAP_SEED 0
-#define GAMMA 1.0
 #define ADAM_ALPHA 1.0 /*same as learning_rate*/
 #define ADAM_BETA1 0.5 /*only adam: between 0 and 1*/
 #define ADAM_BETA2 0.9 /*only adam: between 0 and 1*/
