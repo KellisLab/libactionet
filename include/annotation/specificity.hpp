ec6186f42ee61c4edaff8086a1c09711525e3c21
sebastianpineda <sebastianpineda95@gmail.com>
2024-09-06 14:30:53 -0400

    specificity template test

--- include/annotation/specificity.hpp
+++ include/annotation/specificity.hpp
@@ -6,9 +6,8 @@
 // Exported
 namespace actionet {
 
-    arma::field<arma::mat> compute_feature_specificity(arma::sp_mat &S, arma::mat &H, int thread_no = 0);
-
-    arma::field<arma::mat> compute_feature_specificity(arma::mat &S, arma::mat &H, int thread_no = 0);
+    template <typename T>
+    arma::field<arma::mat> compute_feature_specificity(T &S, arma::mat &H, int thread_no = 0);
 
     template <typename T>
     arma::field<arma::mat> compute_feature_specificity(T &S, arma::uvec sample_assignments, int thread_no = 0);
--- src/annotation/specificity.cpp
+++ src/annotation/specificity.cpp
@@ -1,28 +1,28 @@
 #include "annotation/specificity.hpp"
 #include "utils_internal/utils_matrix.hpp"
 
-arma::field<arma::mat> getProbs(arma::mat& S) {
+arma::field<arma::mat> getProbsObs(const arma::mat& S, arma::mat& Ht, int thread_no) {
+    stderr_printf("Using dense ... ");
+
     arma::mat Sb = S;
     arma::uvec nnz_idx = arma::find(Sb > 0);
     (Sb(nnz_idx)).ones();
 
+    arma::vec col_p = arma::vec(arma::trans(arma::sum(Sb, 0)));
     arma::vec row_p = arma::vec(arma::sum(Sb, 1));
-    arma::vec row_factor = arma::vec(arma::sum(S, 1)) / row_p; // mean of nonzero elements
-    row_p /= Sb.n_cols;
-    arma::vec col_p = arma::vec(arma::trans(arma::mean(Sb, 0)));
+    arma::vec row_factor = arma::vec(arma::sum(S, 1));
 
-    arma::field<arma::mat> out(3);
-    out(0) = col_p;
-    out(1) = row_p;
-    out(2) = row_factor;
+    arma::field<arma::mat> out(4);
+    out(0) = row_factor / row_p; // mean of nonzero elements
+    out(1) = row_p / S.n_cols;
+    out(2) = col_p / S.n_rows;
+    out(3) = S * Ht;
 
     return (out);
 }
 
-arma::field<arma::mat> getProbs(arma::sp_mat& S) {
-    // make sure all values are positive
-    double min_val = S.min();
-    S.for_each([min_val](arma::mat::elem_type& val) { val -= min_val; });
+arma::field<arma::mat> getProbsObs(const arma::sp_mat& S, arma::mat& Ht, int thread_no) {
+    stderr_printf("Using sparse ... ");
 
     // Heuristic optimization! Shall add parallel for later on
     arma::vec row_p = arma::zeros(S.n_rows);
@@ -36,36 +36,37 @@ arma::field<arma::mat> getProbs(arma::sp_mat& S) {
         row_p[it.row()]++;
         row_factor[it.row()] += (*it);
     }
-    row_factor /= row_p;
-    row_p /= S.n_cols;
-    col_p /= S.n_rows;
 
-    arma::field<arma::mat> out(3);
-    out(0) = col_p;
-    out(1) = row_p;
-    out(2) = row_factor;
+    arma::field<arma::mat> out(4);
+    out(0) = row_factor / row_p; // mean of nonzero elements
+    out(1) = row_p / S.n_cols;
+    out(2) = col_p / S.n_rows;
+    out(3) = spmat_mat_product_parallel(S, Ht, thread_no);
 
     return (out);
 }
 
 namespace actionet {
-    arma::field<arma::mat> compute_feature_specificity(arma::sp_mat& S, arma::mat& H, int thread_no) {
+    template <typename T>
+    arma::field<arma::mat> compute_feature_specificity(T& S, arma::mat& H, int thread_no) {
         stdout_printf("Computing feature specificity ... ");
-        stderr_printf("Using sparse ... ");
+
+        // make sure all values are positive
+        double min_val = S.min();
+        S.for_each([min_val](arma::mat::elem_type& val) { val -= min_val; });
+
         arma::mat Ht = arma::trans(H);
         Ht.each_col([](arma::vec& h) {
             double mu = arma::mean(h);
             h /= (mu == 0) ? 1 : mu;
         }); // For numerical stability
 
-        arma::field<arma::mat> p = getProbs(S);
+        arma::field<arma::mat> p = getProbsObs(S, Ht, thread_no);
 
-        arma::vec col_p = p(0);
-        arma::vec row_p = p(1);
-        arma::vec row_factor = p(2);
-
-        // arma::mat Obs = spmat_mat_product_parallel(S, Ht, thread_no);
-        arma::mat Obs = arma::mat(S * arma::sp_mat(Ht));
+        arma::vec row_factor = p(0);
+        arma::vec col_p = p(1);
+        arma::vec row_p = p(2);
+        arma::mat Obs = p(3);
 
         double rho = arma::mean(col_p);
         arma::vec beta = col_p / rho; // Relative density compared to the overall density
@@ -105,60 +106,9 @@ namespace actionet {
         return (res);
     }
 
-    arma::field<arma::mat> compute_feature_specificity(arma::mat& S, arma::mat& H, int thread_no) {
-        stdout_printf("Computing feature specificity ... ");
-
-        arma::mat Ht = arma::trans(H);
-        Ht.each_col([](arma::vec& h) {
-            double mu = arma::mean(h);
-            h /= (mu == 0) ? 1 : mu;
-        }); // For numerical stability
-
-        arma::field<arma::mat> p = getProbs(S);
-
-        arma::vec col_p = p(0);
-        arma::vec row_p = p(1);
-        arma::vec row_factor = p(2);
-
-        arma::mat Obs = S * Ht;
-
-        double rho = arma::mean(col_p);
-        arma::vec beta = col_p / rho; // Relative density compared to the overall density
-        arma::mat Gamma = Ht;
-        arma::vec a(H.n_rows);
-        for (int i = 0; i < H.n_rows; i++) {
-            Gamma.col(i) %= beta;
-            a(i) = arma::max(Gamma.col(i));
-        }
-
-        arma::mat Exp = (row_p % row_factor) * arma::sum(Gamma, 0);
-        arma::mat Nu = (row_p % arma::square(row_factor)) * arma::sum(arma::square(Gamma), 0);
-        arma::mat A = (row_factor * arma::trans(a));
-        arma::mat Lambda = Obs - Exp;
-
-        arma::mat logPvals_lower = arma::square(Lambda) / (2 * Nu);
-        arma::uvec uidx = arma::find(Lambda >= 0);
-        logPvals_lower(uidx) = arma::zeros(uidx.n_elem);
-        logPvals_lower.replace(arma::datum::nan, 0); // replace each NaN with 0
-
-        arma::mat logPvals_upper = arma::square(Lambda) / (2 * (Nu + (Lambda % A / 3)));
-        arma::uvec lidx = arma::find(Lambda <= 0);
-        logPvals_upper(lidx) = arma::zeros(lidx.n_elem);
-        logPvals_upper.replace(arma::datum::nan, 0); // replace each NaN with 0
-
-        logPvals_lower /= log(10);
-        logPvals_upper /= log(10);
-
-        stdout_printf("done\n");
-        FLUSH;
-
-        arma::field<arma::mat> res(3);
-        res(0) = Obs / Ht.n_rows;
-        res(1) = logPvals_upper;
-        res(2) = logPvals_lower;
-
-        return (res);
-    }
+    template arma::field<arma::mat> compute_feature_specificity<arma::mat>(arma::mat& S, arma::mat& H, int thread_no);
+    template arma::field<arma::mat> compute_feature_specificity<arma::sp_mat>(
+        arma::sp_mat& S, arma::mat& H, int thread_no);
 
     template <typename T>
     arma::field<arma::mat> compute_feature_specificity(T& S, arma::uvec sample_assignments, int thread_no) {
