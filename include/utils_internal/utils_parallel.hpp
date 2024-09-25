3c945a8b89e04cdc8f2f788d568297cd543effa1
sebastianpineda <spineda@mit.edu>
2024-09-25 00:59:57 -0700

    Update utils

--- include/utils_internal/utils_parallel.hpp
+++ include/utils_internal/utils_parallel.hpp
@@ -12,14 +12,17 @@ inline unsigned int get_max_threads() {
 
 const unsigned int SYS_THREADS_DEF = get_max_threads();
 
-inline unsigned get_num_threads(const unsigned int max_threads, const unsigned int thread_no = 0) {
+inline unsigned get_num_threads(unsigned int max_threads = 0, const unsigned int thread_no = 0) {
+
+    max_threads = (max_threads > 0) ? std::min(max_threads, SYS_THREADS_DEF) : SYS_THREADS_DEF;
+
     unsigned int threads_use;
 
     if (thread_no <= 0) {
-        threads_use = std::min(SYS_THREADS_DEF, max_threads);
+        threads_use = max_threads;
     }
     else if (thread_no > 1) {
-        threads_use = std::min(thread_no, SYS_THREADS_DEF);
+        threads_use = std::min(thread_no, max_threads);
     }
     else {
         threads_use = 1;
