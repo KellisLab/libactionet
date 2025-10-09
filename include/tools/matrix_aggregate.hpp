e5a884216391f742c0946922f3180393f49a0a4f
sebastianpineda <sebastianpineda95@gmail.com>
2025-10-09 00:43:51 -0400

    FIx sparse aggergate.

--- include/tools/matrix_aggregate.hpp
+++ include/tools/matrix_aggregate.hpp
@@ -6,15 +6,17 @@
 // Exported
 namespace actionet {
     arma::mat computeGroupedSums(arma::sp_mat& S, arma::vec& sample_assignments, int axis = 0);
-
+    arma::sp_mat computeGroupedSums2(arma::sp_mat& S, arma::vec& sample_assignments, int axis = 0);
+    
     arma::mat computeGroupedSums(arma::mat& S, arma::vec& sample_assignments, int axis = 0);
-
+    
     template <typename T>
     arma::mat computeGroupedMeans(T& S, arma::vec& sample_assignments, int axis = 0);
-
+    arma::sp_mat computeGroupedMeans2(arma::sp_mat& S, arma::vec& sample_assignments, int axis = 0);
+    
     arma::mat computeGroupedVars(arma::sp_mat& S, arma::vec& sample_assignments, int axis = 0);
-
     arma::mat computeGroupedVars(arma::mat& S, arma::vec& sample_assignments, int axis = 0);
+    arma::sp_mat computeGroupedVars2(arma::sp_mat& S, arma::vec& sample_assignments, int axis = 0);
 } // namespace actionet
 
 #endif //ACTIONET_MATRIX_MISC_HPP
--- src/tools/matrix_aggregate.cpp
+++ src/tools/matrix_aggregate.cpp
@@ -1,6 +1,7 @@
 #include "tools/matrix_aggregate.hpp"
 
 namespace actionet {
+
     arma::mat computeGroupedSums(arma::sp_mat& S, arma::vec& sample_assignments, int axis) {
         if ((axis == 0 && sample_assignments.n_elem != S.n_cols) || (axis == 1 && sample_assignments.n_elem != S.n_rows)) {
             throw std::invalid_argument("Length of 'sample_assignments' must match the number of columns (axis=0) or rows (axis=1) of S.");
@@ -63,6 +64,34 @@ namespace actionet {
         }
     }
 
+    arma::sp_mat computeGroupedSums2(arma::sp_mat& S, arma::vec& sample_assignments, int axis) {
+        if ((axis == 0 && sample_assignments.n_elem != S.n_cols) || (axis == 1 && sample_assignments.n_elem != S.n_rows)) {
+            throw std::invalid_argument("Length of sample_assignments must match the number of columns (axis=0) or rows (axis=1) of S.");
+        }
+        arma::vec lv_vec = arma::unique(sample_assignments);
+        if (axis == 0) {
+            arma::sp_mat pb(S.n_rows, lv_vec.n_elem);
+            arma::sp_mat::const_iterator it = S.begin();
+            arma::sp_mat::const_iterator it_end = S.end();
+            for (; it != it_end; ++it) {
+                int i = it.row();
+                int j = sample_assignments[it.col()] - 1;
+                pb(i, j) += (*it);
+            }
+            return pb;
+        } else {
+            arma::sp_mat pb(lv_vec.n_elem, S.n_cols);
+            arma::sp_mat::const_iterator it = S.begin();
+            arma::sp_mat::const_iterator it_end = S.end();
+            for (; it != it_end; ++it) {
+                int i = sample_assignments[it.row()] - 1;
+                int j = it.col();
+                pb(i, j) += (*it);
+            }
+            return pb;
+        }
+    }
+
     template <typename T>
     arma::mat computeGroupedMeans(T& S, arma::vec& sample_assignments, int axis) {
         if ((axis == 0 && sample_assignments.n_elem != S.n_cols) || (axis == 1 && sample_assignments.n_elem != S.n_rows)) {
@@ -85,6 +114,54 @@ namespace actionet {
 
     template arma::mat computeGroupedMeans<arma::mat>(arma::mat& S, arma::vec& sample_assignments, int axis);
     template arma::mat computeGroupedMeans<arma::sp_mat>(arma::sp_mat& S, arma::vec& sample_assignments, int axis);
+
+    arma::sp_mat computeGroupedMeans2(arma::sp_mat& S, arma::vec& sample_assignments, int axis) {
+        if ((axis == 0 && sample_assignments.n_elem != S.n_cols) || (axis == 1 && sample_assignments.n_elem != S.n_rows)) {
+            throw std::invalid_argument("Length of sample_assignments must match the number of columns (axis=0) or rows (axis=1) of S.");
+        }
+        arma::vec lv_vec = arma::unique(sample_assignments);
+        if (axis == 0) {
+            arma::sp_mat pb(S.n_rows, lv_vec.n_elem);
+            arma::sp_mat pbz(S.n_rows, lv_vec.n_elem);
+            arma::sp_mat::const_iterator it = S.begin();
+            arma::sp_mat::const_iterator it_end = S.end();
+            for (; it != it_end; ++it) {
+                int i = it.row();
+                int j = sample_assignments[it.col()] - 1;
+                pb(i, j) += (*it);
+                pbz(i, j) += 1;
+            }
+            for (int j = 0; j < pb.n_cols; ++j) {
+                arma::uvec idx = arma::find(sample_assignments == (j + 1));
+                int nnz = (int)idx.n_elem;
+                double denom = std::max(1, nnz);
+                for (int i = 0; i < pb.n_rows; ++i) {
+                    if (pb(i, j) != 0.0) pb(i, j) /= denom;
+                }
+            }
+            return pb;
+        } else {
+            arma::sp_mat pb(lv_vec.n_elem, S.n_cols);
+            arma::sp_mat pbz(lv_vec.n_elem, S.n_cols);
+            arma::sp_mat::const_iterator it = S.begin();
+            arma::sp_mat::const_iterator it_end = S.end();
+            for (; it != it_end; ++it) {
+                int i = sample_assignments[it.row()] - 1;
+                int j = it.col();
+                pb(i, j) += (*it);
+                pbz(i, j) += 1;
+            }
+            for (int i = 0; i < pb.n_rows; i++) {
+                arma::uvec idx = arma::find(sample_assignments == (i + 1));
+                int nnz = (int)idx.n_elem;
+                double denom = std::max(1, nnz);
+                for (int j = 0; j < pb.n_cols; ++j) {
+                    if (pb(i, j) != 0.0) pb(i, j) /= denom;
+                }
+            }
+            return pb;
+        }
+    }
 
     arma::mat computeGroupedVars(arma::sp_mat& S, arma::vec& sample_assignments, int axis) {
         if ((axis == 0 && sample_assignments.n_elem != S.n_cols) || (axis == 1 && sample_assignments.n_elem != S.n_rows)) {
@@ -171,4 +248,65 @@ namespace actionet {
             return pb;
         }
     }
+
+arma::sp_mat computeGroupedVars2(arma::sp_mat& S, arma::vec& sample_assignments, int axis) {
+    if ((axis == 0 && sample_assignments.n_elem != S.n_cols) || (axis == 1 && sample_assignments.n_elem != S.n_rows)) {
+        throw std::invalid_argument("Length of sample_assignments must match the number of columns (axis=0) or rows (axis=1) of S.");
+    }
+    arma::vec lv_vec = arma::unique(sample_assignments);
+    if (axis == 0) {
+        arma::sp_mat mean_pb = computeGroupedMeans2(S, sample_assignments, axis);
+        arma::sp_mat pb(S.n_rows, lv_vec.n_elem);
+        arma::sp_mat pbz(S.n_rows, lv_vec.n_elem);
+        arma::sp_mat::const_iterator it = S.begin();
+        arma::sp_mat::const_iterator it_end = S.end();
+        for (; it != it_end; ++it) {
+            int i = it.row();
+            int j = sample_assignments[it.col()] - 1;
+            double num = (*it) - mean_pb(i, j);
+            pb(i, j) += num * num;
+            pbz(i, j) += 1;
+        }
+        for (int j = 0; j < pb.n_cols; ++j) {
+            arma::uvec idx = arma::find(sample_assignments == (j + 1));
+            int nnz = (int)idx.n_elem;
+            for (int i = 0; i < pb.n_rows; ++i) {
+                int nz = nnz - pbz(i, j);
+                pb(i, j) += nz * mean_pb(i, j) * mean_pb(i, j);
+            }
+            double denom = std::max(1, nnz - 1);
+            for (int i = 0; i < pb.n_rows; ++i) {
+                if (pb(i, j) != 0.0) pb(i, j) /= denom;
+            }
+        }
+        return pb;
+    } else {
+        arma::sp_mat mean_pb = computeGroupedMeans2(S, sample_assignments, axis);
+        arma::sp_mat pb(lv_vec.n_elem, S.n_cols);
+        arma::sp_mat pbz(lv_vec.n_elem, S.n_cols);
+        arma::sp_mat::const_iterator it = S.begin();
+        arma::sp_mat::const_iterator it_end = S.end();
+        for (; it != it_end; ++it) {
+            int i = sample_assignments[it.row()] - 1;
+            int j = it.col();
+            double num = (*it) - mean_pb(i, j);
+            pb(i, j) += num * num;
+            pbz(i, j) += 1;
+        }
+        for (int i = 0; i < pb.n_rows; ++i) {
+            arma::uvec idx = arma::find(sample_assignments == (i + 1));
+            int nnz = (int)idx.n_elem;
+            for (int j = 0; j < pb.n_cols; ++j) {
+                int nz = nnz - pbz(i, j);
+                pb(i, j) += nz * mean_pb(i, j) * mean_pb(i, j);
+            }
+            double denom = std::max(1, nnz - 1);
+            for (int j = 0; j < pb.n_cols; ++j) {
+                if (pb(i, j) != 0.0) pb(i, j) /= denom;
+            }
+        }
+        return pb;
+    }
+}
+
 } // namespace actionet
